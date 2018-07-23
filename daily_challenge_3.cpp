aa825cbbdf2676d59a7e90a4b24ea65534dc1ed2
aswathikb <aswathikb2018@gmail.com>
2018-07-23 20:26:22 +0530

    challenge2

---
+++ daily_challenge_3.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    char s[26];
+    int l,n;
+    cin>>s;
+    l = strlen(s);
+    n = (l*25) + 26;
+    cout<<n;
+	// your code goes here
+	return 0;
+}
